9a27698e6a6dfc59fefbfd1adb94b2ff162908ed
Bern <bernnystrom@gmail.com>
2021-03-30 15:34:59 +0800

    Import firebase config into main.js file

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import VeeValidatePlugin from './includes/validation';
+import './includes/firebase';
 import './assets/tailwind.css';
 import './assets/main.css';
 
