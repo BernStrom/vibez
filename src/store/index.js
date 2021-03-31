fd0df4d7d937f9208e98629f9163f177e75f8b67
Bern <bernnystrom@gmail.com>
2021-03-31 17:09:32 +0800

    Add an action for the user registration that takes in the user data payload

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
   state: {
@@ -15,5 +16,22 @@ export default createStore({
   },
   getters: {
     // authModalShow: (state) => state.authModalShow,
+  },
+  actions: {
+    async register({ commit }, payload) {
+      await auth.createUserWithEmailAndPassword(
+        payload.email,
+        payload.password,
+      );
+
+      await usersCollection.add({
+        name: payload.name,
+        email: payload.email,
+        age: payload.age,
+        country: payload.country,
+      });
+
+      commit('toggleAuth');
+    },
   },
 });
