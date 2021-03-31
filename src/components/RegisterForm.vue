c75a77cc9c58e44be5d9c870c52a8d7fa79c780e
Bern <bernnystrom@gmail.com>
2021-03-31 14:35:30 +0800

    Add creation of user data in firestore db collection from user registration

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -113,7 +113,7 @@
 </template>
 
 <script>
-import { auth } from '@/includes/firebase';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default {
   name: 'RegisterForm',
@@ -150,6 +150,20 @@ export default {
           values.email,
           values.password,
         );
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = 'bg-red-500';
+        this.reg_alert_msg = 'An unexpected error occurred. Please try again later.';
+        return;
+      }
+
+      try {
+        await usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country,
+        });
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
