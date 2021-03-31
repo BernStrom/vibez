d23d3f41e670bf3c72564d97f520c8d5335bc68e
Bern <bernnystrom@gmail.com>
2021-03-31 17:10:42 +0800

    Refactor register method to use an action dispatch instead on user registration

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -113,8 +113,6 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase';
-
 export default {
   name: 'RegisterForm',
   data() {
@@ -144,12 +142,8 @@ export default {
       this.reg_alert_variant = 'bg-blue-500';
       this.reg_alert_msg = 'Please wait! Your account is being created.';
 
-      let userCred = null;
       try {
-        userCred = await auth.createUserWithEmailAndPassword(
-          values.email,
-          values.password,
-        );
+        await this.$store.dispatch('register', values);
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = 'bg-red-500';
@@ -157,24 +151,8 @@ export default {
         return;
       }
 
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country,
-        });
-      } catch (error) {
-        this.reg_in_submission = false;
-        this.reg_alert_variant = 'bg-red-500';
-        this.reg_alert_msg = 'An unexpected error occurred. Please try again later.';
-        return;
-      }
-
-      this.$store.commit('toggleAuth');
       this.reg_alert_variant = 'bg-green-500';
       this.reg_alert_msg = 'Success! Your account has been created.';
-      console.log(userCred);
     },
   },
 };
