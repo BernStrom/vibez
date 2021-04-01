75a9e26a4845eea81f87b71d459d1ce595e67653
Bern <bernnystrom@gmail.com>
2021-04-01 14:42:44 +0800

    Change normal link tags to use router-link component to binds to the matching named component page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,22 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Vibez</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        :to="{ name: 'home' }"
+        exact-active-class="no-active"
+        >Vibez</router-link
+      >
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }"
+              >About</router-link
+            >
+          </li>
           <li v-if="!userLoggedIn">
             <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
               >Login / Register</a
@@ -16,7 +26,9 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }"
+                >Manage</router-link
+              >
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="signOut"
