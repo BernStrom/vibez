d91b55ba66b9162d9b223901b718b5d6bd78115f
Bern <bernnystrom@gmail.com>
2021-04-01 14:39:55 +0800

    Add routes to home, about and manage with active link tailwind class

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
+import Manage from '@/views/Manage.vue';
 
-const routes = [];
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: Home,
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: About,
+  },
+  {
+    name: 'manage',
+    path: '/manage',
+    component: Manage,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
