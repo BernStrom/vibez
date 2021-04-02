7b3d41bddd3604ba1dc05e2691c74190df5b1803
Bern <bernnystrom@gmail.com>
2021-04-02 18:22:20 +0800

    Update route query to dynamically change based on the sort order of comments and dynamically update the comment count

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -26,7 +26,7 @@
     <div class="bg-white rounded border border-gray-200 relative flex flex-col">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <!-- Comment Count -->
-        <span class="card-title">Comments (15)</span>
+        <span class="card-title">{{ song.comment_count }}</span>
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
@@ -128,6 +128,9 @@ export default {
       return;
     }
 
+    const { sort } = this.$route.query;
+    this.sort = sort === '1' || sort === '2' ? sort : '1';
+
     this.song = docSnapshot.data();
     this.getComments();
   },
@@ -147,6 +150,10 @@ export default {
       };
 
       await commentsCollection.add(comment);
+      this.song.comment_count += 1;
+      await songsCollection
+        .doc(this.$route.params.id)
+        .update({ comment_count: this.song.comment_count });
 
       this.getComments();
       this.comment_in_submission = false;
@@ -166,6 +173,16 @@ export default {
           docID: doc.id,
           ...doc.data(),
         });
+      });
+    },
+  },
+  watch: {
+    sort(newVal) {
+      if (newVal === this.$route.query.sort) return;
+      this.$router.push({
+        query: {
+          sort: newVal,
+        },
       });
     },
   },
