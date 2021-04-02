e564aa94c40a465f28f4e6d5792d1a68a6c3edb2
Bern <bernnystrom@gmail.com>
2021-04-02 13:48:27 +0800

    Update routes with redirects and conditional renders based on user authentication

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
 import Manage from '@/views/Manage.vue';
+import store from '@/store';
 
 const routes = [
   {
@@ -16,8 +17,23 @@ const routes = [
   },
   {
     name: 'manage',
-    path: '/manage',
+    path: '/manage-music',
+    meta: {
+      requiresAuth: true,
+    },
     component: Manage,
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
@@ -25,6 +41,19 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
   linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
