781129db78027a37bec5e17ab65dcfc87a6188d8
Bern <bernnystrom@gmail.com>
2021-04-02 17:07:29 +0800

    Replace link tag with router-link component from vue router with dynamic params to the firestore document ID of the song

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -4,7 +4,11 @@
       duration-300 hover:bg-gray-50"
   >
     <div>
-      <a href="#" class="font-bold block text-gray-600">{{ song.modified_name }}</a>
+      <router-link
+        :to="{ name: 'song', params: { id: song.docID } }"
+        class="font-bold block text-gray-600"
+        >{{ song.modified_name }}</router-link
+      >
       <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
     </div>
 
