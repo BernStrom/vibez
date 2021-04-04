9404e02760285e9a675bafdde99f9dd166e386d2
Bern <bernnystrom@gmail.com>
2021-04-04 17:36:24 +0800

    Fix icon directives

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,7 +31,7 @@
       >
         <div
           class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
-          v-icon.right.yellow="'headphones-alt'"
+          v-icon-secondary="{ icon: 'headphones-alt', right: true }"
         >
           <span class="card-title">Songs</span>
         </div>
@@ -48,11 +48,15 @@
 <script>
 import { songsCollection } from '@/includes/firebase';
 import AppSongItem from '@/components/SongItem.vue';
+import IconSecondary from '@/directives/icon-secondary';
 
 export default {
   name: 'Home',
   components: {
     AppSongItem,
+  },
+  directives: {
+    'icon-secondary': IconSecondary,
   },
   data() {
     return {
