c98aebf89ea0f10afc7f630cf61a7635bd2beaf2
Bern <bernnystrom@gmail.com>
2021-04-04 18:30:13 +0800

    Successfully passed tests for Home and About page

---
+++ tests/unit/home.spec.js
@@ -0,0 +1,32 @@
+import Home from '@/views/Home.vue';
+import SongItem from '@/components/SongItem.vue';
+import { shallowMount } from '@vue/test-utils';
+
+jest.mock('@/includes/firebase', () => {});
+
+describe('Home.vue', () => {
+  test('renders list of songs', () => {
+    const songs = [{}, {}, {}];
+
+    Home.methods.getSongs = () => false;
+
+    const component = shallowMount(Home, {
+      data() {
+        return {
+          songs,
+        };
+      },
+      global: {
+        mocks: {
+          $t: (message) => message,
+        },
+      },
+    });
+
+    const items = component.findAllComponents(SongItem);
+    expect(items).toHaveLength(songs.length);
+    items.forEach((wrapper, i) => {
+      expect(wrapper.props().song).toBe(songs[i]);
+    });
+  });
+});
