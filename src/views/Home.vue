04ff3dbfa62769b95d4a7e1f40b6ce0b80200f6c
Bern <bernnystrom@gmail.com>
2021-04-05 14:32:57 +0800

    Replace app description placeholder text in Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,10 +10,9 @@
         <div class="text-white main-header-content">
           <h1 class="font-bold text-5xl mb-5">{{ $t('home.listen') }}</h1>
           <p class="w-full md:w-8/12 mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            et dolor mollis, congue augue non, venenatis elit. Nunc justo eros,
-            suscipit ac aliquet imperdiet, venenatis et sapien. Duis sed magna
-            pulvinar, fringilla lorem eget, ullamcorper urna.
+            Share, comment, and connect with fellow Viberz in a singular space.
+            Bringing together your favorite tunes and building a like-minded
+            community that vibe on a similar acoustic level.
           </p>
         </div>
       </div>
